9dc1930506e2f11eafbee0e2a16d5ccbf4ebd9f0
Tahsin Alahi <alahitahsin@gmail.com>
2025-03-18 14:43:54 +0600

    refactor: add colors and fonts to homepage

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router";
 
 function RootLayout() {
   return (
-    <>
+    <div className="font-poppins">
       <Outlet />
-    </>
+    </div>
   );
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ function HomePage() {
       <main className="max-w-screen-2xl h-full mx-auto">
         <div className="h-full max-w-xl  mx-auto flex items-center justify-center flex-col text-white">
           <div className="text-center space-y-5">
-            <h1 className="md:text-4xl text-2xl font-bold">
+            <h1 className="md:text-4xl text-2xl font-bold font-rubik">
               Welcome to HandsOn
             </h1>
             <p className="text-lg w-11/12 mx-auto px-3 font-poppins">
@@ -26,13 +26,13 @@ function HomePage() {
           <div className="space-x-3 text-lg mt-7">
             <Link
               to="/login"
-              className="font-semibold hover:text-white px-5 py-1 pb-2 rounded-lg transition duration-300 ease-in-out text-center"
+              className="font-semibold bg-accent text-black hover:bg-secondary hover:text-white px-5 py-1  rounded-lg transition duration-300 ease-in-out text-center"
             >
               Login
             </Link>
             <Link
               to="/register"
-              className="font-semibold hover:text-white px-5 py-1 pb-2 rounded-lg transition duration-300 ease-in-out text-center"
+              className="font-semibold text-black bg-accent hover:bg-secondary hover:text-white px-5 py-1 rounded-lg transition duration-300 ease-in-out text-center"
             >
               Sign up
             </Link>
